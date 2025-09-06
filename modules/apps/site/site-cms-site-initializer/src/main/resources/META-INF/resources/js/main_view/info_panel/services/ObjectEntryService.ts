a189fab8ddcdbcd036b5e281a35da18361672d9d
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2025-09-06 16:12:35 +0200

    LPD-62567 Enhanced Categorization type

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/info_panel/services/ObjectEntryService.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/info_panel/services/ObjectEntryService.ts
@@ -6,8 +6,10 @@
 import ApiHelper, {RequestResult} from '../../../common/services/ApiHelper';
 import {IAssetObjectEntry} from '../../../structure_builder/types/AssetType';
 
-export type Categorization = {
+export type EntryCategorizationDTO = {
 	keywords?: IAssetObjectEntry['keywords'];
+	lastAddedBrief?: any;
+	taxonomyCategoryBriefs?: IAssetObjectEntry['taxonomyCategoryBriefs'];
 	taxonomyCategoryIds?: IAssetObjectEntry['taxonomyCategoryIds'];
 };
 
@@ -18,7 +20,7 @@ async function getObjectEntry(
 }
 
 async function patchObjectEntry(
-	data: Categorization,
+	data: EntryCategorizationDTO,
 	url: string
 ): Promise<RequestResult<IAssetObjectEntry>> {
 	return await ApiHelper.patch(
