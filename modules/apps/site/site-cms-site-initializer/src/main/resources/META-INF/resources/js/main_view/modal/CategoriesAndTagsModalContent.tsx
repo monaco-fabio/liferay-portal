a00c2f59df22ae22b52e25107b3a4e2e041a9d6a
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2025-09-06 16:13:01 +0200

    LPD-62567 Categories and Tags Modal to add categories and tags in bulk

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/modal/CategoriesAndTagsModalContent.tsx
@@ -0,0 +1,131 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayAlert from '@clayui/alert';
+import ClayButton from '@clayui/button';
+import ClayModal from '@clayui/modal';
+import {sub} from 'frontend-js-web';
+import React, {useCallback, useState} from 'react';
+
+import AssetCategories from '../info_panel/components/AssetCategories';
+import AssetTags from '../info_panel/components/AssetTags';
+import {EntryCategorizationDTO} from '../info_panel/services/ObjectEntryService';
+
+export default function CategoriesAndTagsModalContent({
+	closeModal,
+	cmsGroupId,
+	selectedData,
+}: {
+	closeModal: () => void;
+	cmsGroupId: number;
+	selectedData: any;
+}) {
+	const [categorizationDTO, setCategorizationDTO] =
+		useState<EntryCategorizationDTO>({
+			keywords: [],
+			taxonomyCategoryIds: [],
+			taxonomyCategoryBriefs: [],
+		});
+
+	const doBulkSubmit = useCallback(async () => {
+		closeModal();
+	}, [categorizationDTO, selectedData]);
+
+	const updateLocalObjectEntry = useCallback(
+		({
+			keywords,
+			lastAddedBrief,
+			taxonomyCategoryIds,
+		}: EntryCategorizationDTO): void => {
+			setCategorizationDTO(
+				({
+					keywords: currentKeywords,
+					taxonomyCategoryBriefs = [],
+					taxonomyCategoryIds: currentTaxonomyCategoryIds,
+				}) => ({
+					keywords: keywords || currentKeywords!,
+					taxonomyCategoryBriefs: [
+						...taxonomyCategoryBriefs,
+						...(lastAddedBrief
+							? [
+									{
+										embeddedTaxonomyCategory:
+											lastAddedBrief,
+									},
+								]
+							: []),
+					],
+					taxonomyCategoryIds:
+						taxonomyCategoryIds || currentTaxonomyCategoryIds,
+				})
+			);
+		},
+		[setCategorizationDTO]
+	);
+
+	return (
+		<>
+			<ClayModal.Header>
+				{Liferay.Language.get('add-categories-and-tags')}
+			</ClayModal.Header>
+
+			<ClayModal.Body>
+				<ClayAlert displayType="info" hideCloseIcon={true}>
+					{Liferay.Language.get(
+						'adding-categories-and-tags-will-preserve-the-ones-already-applied-to-the-currently-selected-assets'
+					)}
+				</ClayAlert>
+
+				<AssetCategories
+					cmsGroupId={cmsGroupId}
+					objectEntry={categorizationDTO}
+					updateObjectEntry={updateLocalObjectEntry}
+				/>
+
+				<AssetTags
+					cmsGroupId={cmsGroupId}
+					objectEntry={categorizationDTO}
+					updateObjectEntry={updateLocalObjectEntry}
+				/>
+			</ClayModal.Body>
+
+			<ClayModal.Footer
+				last={
+					<ClayButton.Group spaced>
+						<ClayButton
+							displayType="secondary"
+							onClick={closeModal}
+						>
+							{Liferay.Language.get('cancel')}
+						</ClayButton>
+
+						<ClayButton
+							disabled={
+								!(
+									categorizationDTO.keywords ||
+									categorizationDTO.taxonomyCategoryIds
+								)
+							}
+							displayType="primary"
+							onClick={doBulkSubmit}
+							type="button"
+						>
+							{selectedData.selectAll
+								? Liferay.Language.get('add-to-all-assets')
+								: selectedData?.items.length === 1
+									? Liferay.Language.get('add-to-1-asset')
+									: sub(
+											Liferay.Language.get(
+												'add-to-x-assets'
+											),
+											selectedData?.items.length
+										)}
+						</ClayButton>
+					</ClayButton.Group>
+				}
+			/>
+		</>
+	);
+}
